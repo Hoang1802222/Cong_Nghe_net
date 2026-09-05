ab88518a1825fa7733be44ee18adca1a5ca33a1a
Le Huy Hoang <hoangvb1802@gmail.com>
2026-09-05 21:08:53 +0700

    lhhLesson05

---
+++ lhhLesson05/lhhLesson05/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using lhhLesson05.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace lhhLesson05.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult lhhAbout()
+        {
+            return View();
+        }
+        public IActionResult lhhRazorCode()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
