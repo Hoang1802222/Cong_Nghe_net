004c9e2d40d24842453632a4cc5c869c205d89f7
Le Huy Hoang <hoangvb1802@gmail.com>
2026-09-09 12:10:35 +0700

    lhhLesson06ne

---
+++ lhhLesson06ne/lhhLesson06ne/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using lhhLesson06ne.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace lhhLesson06ne.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ lhhLesson06ne/lhhLesson06ne/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using lhhLesson06ne.Models;
+namespace lhhLesson06ne.ViewComponents
+
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(int? n)
+        {
+            List<Category> categories = new List<Category>
+            {
+                new Category { Id = 1, Name = "Electronics", Description = "Electronic devices and gadgets" },
+                new Category { Id = 2, Name = "Books", Description = "Various genres of books" },
+                new Category {Id = 3, Name = "Clothing", Description = "Apparel and fashion items" },
+                new Category {Id = 4, Name = "Home & Kitchen", Description = "Household and kitchen items" },
+                new Category {Id = 5, Name = "Sports & Outdoors", Description = "Sports equipment and outdoor gear" },
+            };
+            n = n ?? 0;
+            var search = categories.Where(x => x.Id > n).ToList();
+            return View(search);
+        }
+    }
+}
