ca402ec423843e29a9ff29c52e9d2bc0d9ce4e21
Le Huy Hoang <hoangvb1802@gmail.com>
2026-09-13 22:42:44 +0700

    lhhLesoon07ne

---
+++ lhhLesson04/lhhLesson04/Controllers/lhhProductController.cs
@@ -0,0 +1,79 @@
+﻿using lhhLesson04.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lhhLesson04.Controllers
+{
+    public class lhhProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product()
+                {
+                    ProductID = "1",
+                    ProductName = "Product RX-78-2",
+                    ProductImages = Url.Content("~/images/Product1.jpg"),
+                    ProductPrice = 1000,
+                    ProductSalePrice = 800,
+                    CategoryId = "1",
+                    ProductDescription = "The original Product model.",
+                    ProductStatus = "Available",
+                    ProductCreatedDate = DateTime.Now
+                },
+
+                new Product()
+                {
+                    ProductID = "2",
+                    ProductName = "Product Wing Zero",
+                    ProductImages = Url.Content("~/images/Product2.jpg"),
+                    ProductPrice = 1200,
+                    ProductSalePrice = 1000,
+                    CategoryId = "2",
+                    ProductDescription = "A powerful Product from the Wing series.",
+                    ProductStatus = "Available",
+                    ProductCreatedDate = DateTime.Now
+                },
+
+                new Product()
+                {
+                    ProductID = "3",
+                    ProductName = "Product Unicorn",
+                    ProductImages = Url.Content("~/images/Product3.jpg"),
+                    ProductPrice = 1500,
+                    ProductSalePrice = 1300,
+                    CategoryId = "3",
+                    ProductDescription = "A unique Product with a transforming feature.",
+                    ProductStatus = "Available",
+                    ProductCreatedDate = DateTime.Now
+                }
+            };
+
+            List<Category> categories = new List<Category>
+            {
+                new Category()
+                {
+                    CategoryID = "1",
+                    CategoryName = "Mobile Suit Gundam"
+                },
+
+                new Category()
+                {
+                    CategoryID = "2",
+                    CategoryName = "Gundam Wing"
+                },
+
+                new Category()
+                {
+                    CategoryID = "3",
+                    CategoryName = "Gundam Unicorn"
+                }
+            };
+
+            ViewBag.Products = products;
+            ViewBag.Categories = categories;
+
+            return View();
+        }
+    }
+}
---
+++ lhhLesson04/lhhLesson04/Models/Gundam.cs
@@ -0,0 +1,20 @@
+﻿namespace lhhLesson04.Models
+{
+    public class Category
+    {
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+    public class  Product
+    {
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductImages { get; set; }
+        public int ProductPrice { get; set; } = 0;
+        public int ProductSalePrice { get; set; } = 0;
+        public string CategoryId { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductStatus { get; set; }
+        public DateTime ProductCreatedDate { get; set; }
+    }
+}
